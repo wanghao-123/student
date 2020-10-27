cef966f49a6f40edd82d8867db6fc732732a388c
wanghao-123 <2221775654@qq.com>
2020-10-27 23:16:18 +0800

    1111

--- src/main/java/com/cssl/configurer/Myconfigurer.java
+++ src/main/java/com/cssl/configurer/Myconfigurer.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+//123
 @Configuration
 public class Myconfigurer implements WebMvcConfigurer {
     @Override
